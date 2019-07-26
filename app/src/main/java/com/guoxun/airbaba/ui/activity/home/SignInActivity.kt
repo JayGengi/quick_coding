ec47ba48c6002d28c5b157d44686c563f3f855d6
JayGengi <jaygengi@163.com>
2019-07-26 11:10:18 +0800

    日历init UI

--- app/src/main/java/com/guoxun/airbaba/ui/activity/home/SignInActivity.kt
+++ app/src/main/java/com/guoxun/airbaba/ui/activity/home/SignInActivity.kt
@@ -1,10 +1,11 @@
 package com.guoxun.airbaba.ui.activity.home
 
-import android.content.Intent
 import android.view.View
 import com.guoxun.airbaba.R
 import com.guoxun.airbaba.base.BaseActivity
+import com.guoxun.airbaba.widget.calendar.CalendarEntity
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import java.util.ArrayList
 
 /**
   * @des    签到
@@ -23,7 +24,48 @@ class SignInActivity : BaseActivity(),View.OnClickListener{
 
         mTopBar.setTitle("签到")
         mTopBar.addLeftBackImageButton().setOnClickListener { finish() }
-//        detail.setOnClickListener(this)
+
+        calendar_previous.setOnClickListener(this)
+        calendar_next.setOnClickListener(this)
+        tv_calendar_show.setOnClickListener(this)
+        calendarView!!.setDateListener { year, month ->
+            tv_calendar_show!!.text = String.format("%s 年 %s 月", year, month)
+        }
+
+        var item = CalendarEntity()
+        val baseList = ArrayList<CalendarEntity>()
+        item.data = "2019-07-12"
+        item.is_sign = 1
+        baseList.add(item)
+        item = CalendarEntity()
+        item.data = "2019-07-23"
+        item.is_sign = 1
+        baseList.add(item)
+        item = CalendarEntity()
+        item.data = "2019-07-24"
+        item.is_sign = 2
+        baseList.add(item)
+        item = CalendarEntity()
+        item.data = "2019-07-25"
+        item.is_sign = 1
+        baseList.add(item)
+        item = CalendarEntity()
+        item.data = "2019-08-12"
+        item.is_sign = 2
+        baseList.add(item)
+        item = CalendarEntity()
+        item.data = "2019-08-13"
+        item.is_sign = 1
+        baseList.add(item)
+        item = CalendarEntity()
+        item.data = "2019-08-14"
+        item.is_sign = 1
+        baseList.add(item)
+        item = CalendarEntity()
+        item.data = "2019-08-15"
+        item.is_sign = 2
+        baseList.add(item)
+        calendarView.setSignRecords(baseList)
     }
 
     override fun start() {
@@ -31,10 +73,15 @@ class SignInActivity : BaseActivity(),View.OnClickListener{
     }
     override fun onClick(v: View) {
         when(v.id){
-//            R.id.detail ->{
-//            }
-//            R.id.real_name_lay ->{
-//            }
+            R.id.calendar_previous ->{
+                calendarView.showPreviousMonth()
+            }
+            R.id.tv_calendar_show ->{
+                calendarView!!.selectMonth(2019, 9)
+            }
+            R.id.calendar_next ->{
+                calendarView.showNextMonth()
+            }
         }
     }
     override fun initData() {
---
+++ app/src/main/java/com/guoxun/airbaba/widget/calendar/SignCalendar.java
@@ -0,0 +1,174 @@
+package com.guoxun.airbaba.widget.calendar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * 功能：
+ * Created by 何志伟 on 2017/8/15.
+ */
+
+class SignCalendar extends View {
+
+    private String[] weekTitles = new String[]{"日", "一", "二", "三", "四", "五", "六"};
+    private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.CHINA);
+    private Calendar calendar = Calendar.getInstance();
+    //今天所在的年月日信息
+    private int currentYear, currentMonth, currentDay;
+    private GregorianCalendar date = new GregorianCalendar();
+    private SignCalendarView.Config config;
+    private List<CalendarEntity> signRecords;
+    private Paint paint = new Paint();
+    private LunarHelper lunarHelper;
+    private int itemWidth, itemHeight;
+    private float solarTextHeight;
+    private int currentPosition;
+
+
+    public SignCalendar(Context context) {
+        super(context);
+    }
+
+    public SignCalendar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SignCalendar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    void init(SignCalendarView.Config config) {
+        this.config = config;
+        currentYear = calendar.get(Calendar.YEAR);
+        currentMonth = calendar.get(Calendar.MONTH);
+        currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setStrokeWidth(sp2px(0.6f));
+        currentPosition = (currentYear - 1970) * 12 + currentMonth + 1;
+        setClickable(true);
+        if (config.isShowLunar) lunarHelper = new LunarHelper();
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        itemWidth = getWidth() / 7;
+        itemHeight = (getHeight() - (int) config.weekHeight) / 6;
+        paint.setTextSize(config.calendarTextSize);
+        solarTextHeight = getTextHeight();
+        if (config.signSize == 0) config.signSize = Math.min(itemHeight, itemWidth);
+    }
+
+    final void selectDate(int position) {
+        currentPosition = position - 1;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        paint.setColor(Color.LTGRAY);
+        canvas.drawLine(0, config.weekHeight, 0, getHeight() - config.weekHeight, paint);
+        //画日历顶部周的标题
+        paint.setColor(config.weekBackgroundColor);
+        canvas.drawRect(0, 0, getWidth(), config.weekHeight, paint);
+        paint.setTextSize(config.weekTextSize);
+        paint.setColor(config.weekTextColor);
+        float delay = getTextHeight() / 4;
+        for (int i = 0; i < 7; i++) {
+            canvas.drawText(weekTitles[i], itemWidth * (i + 0.5f), config.weekHeight / 2 + delay, paint);
+        }
+        //画日历
+        int year = 1970 + currentPosition / 12;
+        int month = currentPosition % 12;
+        calendar.set(year, month, 1);
+        int firstDay = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+        int selectMonthMaxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        delay = solarTextHeight / 4;
+        if (config.isShowLunar) delay = 0;
+        for (int i = 1; i <= selectMonthMaxDay; i++) {
+            int position = i - 1 + firstDay;
+            int x = (position % 7) * itemWidth + itemWidth / 2;
+            int y = (position / 7) * itemHeight + itemHeight / 2 + (int) config.weekHeight + (int) delay;
+            int day = i;
+            //签到背景
+            boolean isSign = false;
+            if (signRecords != null) {
+                date.set(year, month, day);
+                String dateStr = format.format(date.getTime());
+
+                for(int da = 0; da<signRecords.size(); da ++){
+                     if(dateStr.equals(signRecords.get(da).getData()) && 1 == signRecords.get(da).getIs_sign()){
+                        paint.setColor(config.signColor);
+                        canvas.drawCircle(x, y, config.signSize / 2, paint);
+                        isSign = true;
+                    }else if(dateStr.equals(signRecords.get(da).getData()) && 2 == signRecords.get(da).getIs_sign()){
+                        paint.setColor(config.errorColor);
+                        canvas.drawCircle(x, y, config.signSize / 2, paint);
+                        isSign = false;
+                    }
+                }
+//                if (signRecords.contains(dateStr)) {
+//                    paint.setColor(config.signColor);
+//                    canvas.drawCircle(x, y, config.signSize / 2, paint);
+//                    isSign = true;
+//                }
+            }
+            //农历
+            if (config.isShowLunar) {
+                paint.setColor(isSign ? config.signTextColor : config.lunarTextColor);
+                String lunar = lunarHelper.SolarToLunarString(year, month + 1, day);
+                paint.setTextSize(config.lunarTextSize);
+                canvas.drawText(lunar, x, y + solarTextHeight * 2 / 3, paint);
+            }
+            //阳历
+            if (isSign) {
+                for(int da = 0; da<signRecords.size(); da ++){
+                    if( 1 == signRecords.get(da).getIs_sign()){
+                        paint.setColor(config.signTextColor);
+                    }
+                }
+//                paint.setColor(config.signTextColor);
+            }else {
+                if (year == currentYear && month == currentMonth && day == currentDay) {//今天
+                    paint.setColor(config.todayTextColor);
+                } else {//其他天
+                    paint.setColor(config.calendarTextColor);
+                }
+                for(int da = 0; da<signRecords.size(); da ++){
+                     if( 2 == signRecords.get(da).getIs_sign()){
+                        paint.setColor(config.errorTextColor);
+                    }
+                }
+            }
+            paint.setTextSize(config.calendarTextSize);
+            canvas.drawText(String.valueOf(day), x, y, paint);
+        }
+    }
+
+    void setSignRecords( List<CalendarEntity> signRecords) {
+        this.signRecords = signRecords;
+    }
+
+    private float sp2px(float spValue) {
+        float fontScale = getContext().getResources().getDisplayMetrics().scaledDensity;
+        return spValue * fontScale + 0.5f;
+    }
+
+    private float getTextHeight() {
+        return paint.descent() - paint.ascent();
+    }
+
+
+}
